73c72770e8f6c7680f7e6f4b9900ebf68f046c86
luisff2016 <luisff@dcomp.ufs.br>
2020-02-17 22:31:55 -0300

    ajustes na localizacao das pastas

---
+++ app_minha_consulta/lib/anexos/mediminder_main.dart
@@ -0,0 +1,37 @@
+import 'package:app_minha_consulta/anexos/src/global_bloc.dart';
+import 'package:app_minha_consulta/anexos/src/ui/homepage/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MedicineReminder());
+}
+
+class MedicineReminder extends StatefulWidget {
+  @override
+  _MedicineReminderState createState() => _MedicineReminderState();
+}
+
+class _MedicineReminderState extends State<MedicineReminder> {
+  GlobalBloc globalBloc;
+
+  void initState() {
+    globalBloc = GlobalBloc();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<GlobalBloc>.value(
+      value: globalBloc,
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          brightness: Brightness.light,
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
