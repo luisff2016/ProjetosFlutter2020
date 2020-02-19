f9a6ed56c49d5f0223e00664936bc6be5faf4141
luisff2016 <luisff@dcomp.ufs.br>
2020-02-19 20:33:03 -0300

    ajuste no main

--- app_minha_consulta/lib/main.dart
+++ app_minha_consulta/lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 
 import 'view/tela_login.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      //home: HomePage(),
-      home: TelaLogin(),
-      //home: TabBarDemo(),
-      )
-  );
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      brightness: Brightness.light,
+    ),
+    //home: HomePage(),
+    home: TelaLogin(),
+    //home: TabBarDemo(),
+  ));
 }
