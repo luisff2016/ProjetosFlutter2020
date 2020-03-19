2663711d8fb1d34546ab855a7fed0ec84f00d06b
luisff2016 <luisff@dcomp.ufs.br>
2020-03-19 00:04:20 -0300

    ajustar importacoes nao usadas

--- app_minha_consulta/lib/main.dart
+++ app_minha_consulta/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:app_minha_consulta/view/tela_login.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:app_minha_consulta/utils.dart' as utils;
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
